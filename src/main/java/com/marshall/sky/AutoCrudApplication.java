0ba784a6b72a4167a0c37c8977506f89fbd1595a
live <wangruigang@ricebook.com>
2019-07-21 00:18:12 +0800

    Initial commit

---
+++ src/main/java/com/marshall/sky/AutoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.marshall.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marshall/sky/AutoCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marshall.sky;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
